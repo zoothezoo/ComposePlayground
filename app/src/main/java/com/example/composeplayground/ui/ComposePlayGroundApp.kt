174701c6db7ad0e49cf759ba8cd7c5287122ee3c
zoothezoo <kohekoshien@gmail.com>
2022-06-10 00:47:35 +0900

    feat: add App

---
+++ app/src/main/java/com/example/composeplayground/ui/ComposePlayGroundApp.kt
@@ -0,0 +1,12 @@
+package com.example.composeplayground.ui
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+
+@Composable
+fun ComposePlayGroundApp(
+
+) {
+
+}
